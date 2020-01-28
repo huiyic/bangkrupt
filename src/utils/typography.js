fabe710599973931186e234a02ab214a4b6989a5
bang <limbanghui@gmail.com>
2020-01-28 20:04:31 +0800

    Add backup font types

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -7,7 +7,7 @@ Wordpress2016.overrideThemeStyles = () => {
       boxShadow: `none`,
     },
     "h1,h2,h3,h4,h5,h6": {
-      fontFamily: "comic sans ms",
+      fontFamily: "Comic Sans MS, helvetica, sans-serif",
     },
   }
 }
