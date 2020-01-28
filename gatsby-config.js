1c40e81e9c684ac2256f78c0511138fd1574d0f9
bang <limbanghui@gmail.com>
2020-01-28 23:44:19 +0800

    Turn on offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `bangkrupt`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
@@ -75,6 +75,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
